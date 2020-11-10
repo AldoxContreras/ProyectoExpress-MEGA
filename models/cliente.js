c5f4cf12ffa04a1e35358e699d15189635f089cb
chrisramfon <chrisramfon@outlook.com>
2020-11-09 23:19:57 -0600

    CRUD para cliente
    Se agrega el schema y las rutas para el crud de cliente.

--- models/cliente.js
+++ models/cliente.js
@@ -1,48 +1,26 @@
-const mongoose = require('mongoose')
-
-mongoose.set('useCreateIndex', true)
+const mongoose = require('mongoose');
+mongoose.set('useCreateIndex', true);
 const ClienteSchema = new mongoose.Schema({
+    id: {type: String},
+    Nombre: {type: String, required: true},
+    Apellido1: {type: String, require: true},
+    Apellido2: {type: String},
+    Direccion: {type: String},
+    Telefono: {type: String},
+    Estado: {type: String},
+    Cita: [{
+        id_cita: {type: Number},
+        Fecha_Hora: {type: Date},
+        Motivo: {type: String},
+        Estado: {type: String},
+        id_Empleado: {type: String},
+        Matricula_Vehiculo: {type: String}
+    }],
+    Vehiculo: [{
+        Matricula: {type: String, unique: true},
+        Marca: {type: String},
+        Modelo: {type: String}
+    }]
 
-    id:{
-        type: String,
-        required: true,
-        unique: true
-    },
-    Nombre:{
-        type: String,
-        required: true
-    },
-    Apellido1:{
-        type: String,
-        required: true,
-    },
-    Apellido2:{
-        type: String,
-        //required: false,
-    },
-    Direccion:{
-        type: String,
-        required: true,
-
-    },
-    Telefono:{
-        type: String,
-        required: true,
-    },
-    Estado:{
-        type: String,
-        required: true,
-    },
-    Cita:{
-        type: Array,
-        required: true,
-        unique: true,
-    },
-    Vehiculo:{
-        type: Array,
-        required: true,
-        unique: true,
-    }
-
-})
-mongoose.model('Cliente', ClienteSchema) //para poner nombre al schema
+});
+mongoose.model('Cliente', ClienteSchema);
