d88fec4dacdd64a5104f7404c8590ad421640652
AldoxContreras <68927132+AldoxContreras@users.noreply.github.com>
2020-11-23 18:19:51 -0600

    Actualizacion y mejoras de formulario de cita
    Se modificó el diseño del formulario. ya registra, solo que falta el autoincremento del id de la cita, se instaló el mongoose-sequence para lo del autoincremento.

--- models/cliente.js
+++ models/cliente.js
@@ -1,28 +1,32 @@
 const mongoose = require('mongoose');
 mongoose.set('useCreateIndex', true);
+var AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const ClienteSchema = new mongoose.Schema({
-    id: {type: String},
-    Nombre: {type: String, required: true},
-    Apellido1: {type: String, require: true},
-    Apellido2: {type: String},
-    Direccion: {type: String},
-    Telefono: {type: String},
-    Estado: {type: String},
+    id: { type: String },
+    Nombre: { type: String, required: true },
+    Apellido1: { type: String, require: true },
+    Apellido2: { type: String },
+    Direccion: { type: String },
+    Telefono: { type: String },
+    Estado: { type: String },
     Cita: [{
-        id_cita: {type: Number, unique: false},
-        Fecha_Hora: {type: Date},
-        Motivo: {type: String},
-        Estado: {type: String},
-        id_Empleado: {type: String},
-        Matricula_Vehiculo: {type: String}
-    }, {sparse: true}],
+        id_cita: { type: Number, unique: false },
+        Fecha_Hora: { type: Date },
+        Motivo: { type: String },
+        Estado: { type: String },
+        id_Empleado: { type: String },
+        Matricula_Vehiculo: { type: String }
+    }, { sparse: true }],
     Vehiculo: [{
-        Matricula: {type: String, unique: false},
-        Marca: {type: String},
-        Modelo: {type: String}
-    }, {unique: false}]
+        Matricula: { type: String, unique: false },
+        Marca: { type: String },
+        Modelo: { type: String }
+    }, { unique: false }]
 
 });
+ClienteSchema.plugin(AutoIncrement, { id_cita: 'order_seq', inc_field: 'order' });
+
+
 mongoose.model('Cliente', ClienteSchema);
-ClienteSchema.index({id_cita: 1}, {sparse: true});
+ClienteSchema.index({ id_cita: 1 }, { sparse: true });
