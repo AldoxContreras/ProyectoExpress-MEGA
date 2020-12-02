fe18f74c603f817b35228352b5ce193a55a67e2e
AldoxContreras <68927132+AldoxContreras@users.noreply.github.com>
2020-12-01 19:38:30 -0600

    Update usuarios.js

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -109,7 +109,7 @@ router.post('/borrar', async(req, res) => {
 });
 
 router.get('/:id', async(req, res) => {
-    usuario = await Usuarios.findOne({ id: req.params.id })
+    let usuario = await Usuarios.findOne({ id: req.params.id })
     if (!usuario) {
         return res.status(404).send("Usuario no encontrado")
     }
